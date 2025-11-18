ee15999cc060643bfc9418ca9f99c249d2311099
sgledhill95 <sam.gledhill@purepetfood.co.uk>
2025-11-18 17:52:25 +0000

    c

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).send(`Hello world!`);
+  res.status(200).send(`Hello world!!`);
 });
 
 app.listen(PORT, () => {
